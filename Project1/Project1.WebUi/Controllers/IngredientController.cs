3c677fd4a70682df5180b325d65036d418f43a3c
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-12 20:59:42 -0600

    Created IngredientController

---
+++ Project1/Project1.WebUi/Controllers/IngredientController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project0.DataAccess.Repositories.Interfaces;
+
+namespace Project1.WebUi.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private IIngredientRepository Repository { get; set; }
+
+        public IngredientController(IIngredientRepository repository)
+        {
+            Repository = repository;
+        }
+
+        // GET: Ingredient
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Ingredient/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Ingredient/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ingredient/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Ingredient/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Ingredient/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Ingredient/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Ingredient/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
